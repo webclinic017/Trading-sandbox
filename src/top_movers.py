18b100b5bdbcfa7ca747ecc72e1e1671fcb8d6d1
BaptisteZloch <bzloch@hotmail.fr>
2023-02-05 15:38:39 +0100

    new files

--- src/top_movers.py
+++ src/top_movers.py
@@ -12,20 +12,22 @@ def get_trading_symbols(limit):
     tickers = client.get_ticker()
 
     for i in range(len(tickers)):
-        tickers[i]['quoteVolume'] = float(tickers[i]['quoteVolume'])
+        tickers[i]["quoteVolume"] = float(tickers[i]["quoteVolume"])
 
-    tickers = sorted(tickers, key=lambda x: x['quoteVolume'], reverse=True)
+    tickers = sorted(tickers, key=lambda x: x["quoteVolume"], reverse=True)
     for item in tickers:
-        symbolpair = item['symbol']
+        symbolpair = item["symbol"]
 
-        if float(item['quoteVolume']) > 50000:
+        if float(item["quoteVolume"]) > 50000:
             list.append(symbolpair)
 
     symbols = list[:limit]
     return symbols
 
+
 top_coins_movers = []
 top_movers = {}
+
 
 def get_movers(qty, move):
     global top_coins_movers
@@ -46,20 +48,26 @@ def get_movers(qty, move):
                 if item_new[0] not in top_movers:
                     top_movers[item_new[0]] = item_old[1] - item_new[1]
                 else:
-                    top_movers[item_new[0]] = top_movers[item_new[0]] + (item_old[1] - item_new[1])
+                    top_movers[item_new[0]] = top_movers[item_new[0]] + (
+                        item_old[1] - item_new[1]
+                    )
 
     top_movers_filtered = {}
     for item in top_movers:
         if top_movers[item] > move:
             top_movers_filtered[item] = top_movers[item]
-    top_movers_filtered_sorted = dict(sorted(top_movers_filtered.items(), key=lambda item: item[1]))
+    top_movers_filtered_sorted = dict(
+        sorted(top_movers_filtered.items(), key=lambda item: item[1])
+    )
 
-    top_movers_filtered_sorted = dict(reversed(list(top_movers_filtered_sorted.items())))
-
+    top_movers_filtered_sorted = dict(
+        reversed(list(top_movers_filtered_sorted.items()))
+    )
 
     return top_movers_filtered_sorted
 
-price_baseline ={}
+
+price_baseline = {}
 top_coins_movers = []
 top_movers = {}
 
@@ -67,10 +75,12 @@ while True:
     top_movers = get_movers(300, 3)
     if top_movers:
         for top_mover in top_movers:
-            AvgPrice = float(client.get_ticker(symbol=top_mover)['weightedAvgPrice'])
+            AvgPrice = float(client.get_ticker(symbol=top_mover)["weightedAvgPrice"])
 
             if top_mover not in price_baseline:
                 price_baseline[top_mover] = AvgPrice
 
             if AvgPrice > price_baseline[top_mover] * 1.02:
-                print(f'Trading pair:  {top_mover}, Price: {AvgPrice}, Price baseline: {price_baseline[top_mover]}, Rank: {top_movers[top_mover]}')
+                print(
+                    f"Trading pair:  {top_mover}, Price: {AvgPrice}, Price baseline: {price_baseline[top_mover]}, Rank: {top_movers[top_mover]}"
+                )
